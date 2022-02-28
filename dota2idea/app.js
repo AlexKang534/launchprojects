3050dcd0f24d3d5dcb5c14488b078a9333728568
AlexKang <85082932+AlexKang534@users.noreply.github.com>
2022-02-28 03:39:58 -0800

    API function
    Added additional API Functionality (Steam ID API returning [object, Object]) Haven't fixed

--- dota2idea/app.js
+++ dota2idea/app.js
@@ -11,32 +11,34 @@ const searchBox = document.querySelector(".steamID");
 searchBox.addEventListener("keypress", setQuery);
 
 //Function to remove string(?)
-function noString() {
-  if (searchBox == `^[A-Za-z]*, $`) {
-    prompt("steamID only ");
-  }
-}
+// function noString(searchBox) {
+//   console.log(searchBox);
+// }
 //functions for API query
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchBox.value);
+    console.log("entered");
   }
 }
 
 //Pull API Data
 function getResults(query) {
   fetch(`https://api.opendota.com/api/players/${query}`)
-    .then((profileurl) => {
-      return profileurl.json();
+    .then((profile) => {
+      return profile.json();
     })
-    .then(displayResults);
+    .then(displayResults)
+    .catch(function (err) {
+      console.log("something wrong with API function");
+    });
 }
 
 //Display JSON Data into appropriate DOM slots
-function displayResults(profileurl) {
-  let profile = document.querySelector(".profile-url .profile");
-  profile.innerText = `${profileurl}`;
-  console.log(profileurl);
+function displayResults(profile) {
+  let profileInformation = document.querySelector(".profile-url .profile");
+  profileInformation.innerText = JSON.stringify(`${profile}`);
+  console.log(profile);
 }
 
 //Event Listeners for Yes/No
